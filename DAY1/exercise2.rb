0125398e34a69dea9ef55cdaea6ec956d97b23ca
Shamiur Rahman <IamShamiurRahman@gmail.com>
2019-08-04 11:15:49 +0530

    Exercise 2 Queue Done

--- DAY1/exercise2.rb
+++ DAY1/exercise2.rb
@@ -1,14 +1,15 @@
 class Queue
+    attr_reader :fifo
     def initialize
-
+        @fifo = []
     end
     def enqueue(el)
-
+       fifo.push(el) 
     end
     def dequeue
-    
+        fifo.shift()
     end
     def peek
-
+        fifo.first
     end
 end
